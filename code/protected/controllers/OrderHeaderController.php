02b261c201f4e5b51a957704c0e9714ec3340925
Kuldeep Bagwari <kuldeep@canbrand.in>
2016-06-03 18:10:08 +0530

    email templet change

--- code/protected/controllers/OrderHeaderController.php
+++ code/protected/controllers/OrderHeaderController.php
@@ -268,13 +268,13 @@ class OrderHeaderController extends Controller {
     <tr style="display: block; margin-top:0px;background: #444; padding: 15px 0;">
       <td colspan="2" style="width: 600px;">
         <ul style="display:block; width:100%; list-style-type:none;overflow: hidden;margin: 0;padding: 10px 0;">
-          <li style="display:block; width:200px; float:left; text-align:center;">
+          <li style="display:block; width:170px; float:left; text-align:center;">
             <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px; border-right:1px solid #676767;">Visit Website</a>
           </li>
-          <li style="display:block; width:200px; float:left; text-align:center;">
+          <li style="display:block; width:170px; float:left; text-align:center;">
             <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px;">Terms &amp; Conditions</a>
           </li>
-          <li style="display:block; width:200px; float:left; text-align:center;">
+          <li style="display:block; width:170px; float:left; text-align:center;">
             <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px; border-left:1px solid #676767;">Privacy Policy</a>
           </li>
         </ul>
@@ -693,13 +693,13 @@ class OrderHeaderController extends Controller {
     <tr style="display: block; margin-top:0px;background: #444; padding: 15px 0;">
       <td colspan="2" style="width: 600px;">
         <ul style="display:block; width:100%; list-style-type:none;overflow: hidden;margin: 0;padding: 10px 0;">
-          <li style="display:block; width:200px; float:left; text-align:center;">
+          <li style="display:block; width:170px; float:left; text-align:center;">
             <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px; border-right:1px solid #676767;">Visit Website</a>
           </li>
-          <li style="display:block; width:200px; float:left; text-align:center;">
+          <li style="display:block; width:170px; float:left; text-align:center;">
             <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px;">Terms &amp; Conditions</a>
           </li>
-          <li style="display:block; width:200px; float:left; text-align:center;">
+          <li style="display:block; width:170px; float:left; text-align:center;">
             <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px; border-left:1px solid #676767;">Privacy Policy</a>
           </li>
         </ul>
@@ -785,13 +785,13 @@ class OrderHeaderController extends Controller {
     <tr style="display: block; margin-top:0px;background: #444; padding: 15px 0;">
       <td colspan="2" style="width: 600px;">
         <ul style="display:block; width:100%; list-style-type:none;overflow: hidden;margin: 0;padding: 10px 0;">
-          <li style="display:block; width:200px; float:left; text-align:center;">
+          <li style="display:block; width:170px; float:left; text-align:center;">
             <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px; border-right:1px solid #676767;">Visit Website</a>
           </li>
-          <li style="display:block; width:200px; float:left; text-align:center;">
+          <li style="display:block; width:170px; float:left; text-align:center;">
             <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px;">Terms &amp; Conditions</a>
           </li>
-          <li style="display:block; width:200px; float:left; text-align:center;">
+          <li style="display:block; width:170px; float:left; text-align:center;">
             <a href="#!" style="display:block;color:#a9a9a9; text-transform:uppercase;text-decoration:none; font-size:14px; border-left:1px solid #676767;">Privacy Policy</a>
           </li>
         </ul>
