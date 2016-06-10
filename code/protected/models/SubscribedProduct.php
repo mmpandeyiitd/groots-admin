5594fc1070209c31e289f575b0363ad32cfc9ad8
Kuldeep Bagwari <kuldeep@canbrand.in>
2016-06-10 15:43:06 +0530

    update query

--- code/protected/models/SubscribedProduct.php
+++ code/protected/models/SubscribedProduct.php
@@ -380,7 +380,7 @@ class SubscribedProduct extends CActiveRecord {
 
     public function update_mrp_wsp($mrp, $wsp, $diameter, $grade, $store_id, $base_product_id, $quantity, $Weight, $WeightUnit, $Length, $LengthUnit, $status) {
        
-       /* if($diameter==''){
+      /* if($diameter==''){
             $diameter='NULL';
         }
         if($grade=='')
@@ -403,6 +403,14 @@ class SubscribedProduct extends CActiveRecord {
         {
            $LengthUnit='NULL';
         }*/
+         if($Weight=='')
+        {
+           $Weight=0; 
+        }
+        if($Length=='')
+        {
+           $Length=0; 
+        }
         $connection = Yii::app()->db;
         $sql = "update subscribed_product set store_offer_price='" . $wsp . "',grade ='" . $grade . "',diameter ='" . $diameter . "',quantity ='" . $quantity . "',store_price='" . $mrp . "', weight='" . $Weight . "',weight_unit='" . $WeightUnit . "',length='" . $Length . " ',status='" . $status . "',length_unit='" . $LengthUnit . "' where base_product_id='" . $base_product_id . "' and store_id='" . $store_id . "'";
         $command = $connection->createCommand($sql);
