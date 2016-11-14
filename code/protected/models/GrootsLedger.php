65d0e5d49eaf2a0977ee57b1359df48d0fab503c
ashu dogra <ashudogra20@gmail.com>
2016-11-14 12:29:53 +0530

    category name added in report

--- code/protected/models/GrootsLedger.php
+++ code/protected/models/GrootsLedger.php
@@ -187,9 +187,11 @@ class GrootsLedger extends CActiveRecord
     public static function downloadCSVByIDs($cDate,$cdate1) {
 
 
-        $sqlchksubsid = "SELECT oh.`delivery_date`as 'Delivery Date',r.name as 'Client Name',bp.title as 'Item Name',bp.pack_size as 'Pack Size', bp.pack_unit as 'Pack Unit', bp.pack_size_in_gm as 'Pack Size In Gm' ,ol.delivered_qty as 'Total Quantity (Kg)',TRUNCATE((ol.unit_price *ol.delivered_qty),2) as 'Total Amount', oh.`invoice_number` as 'Invoice ID', oh.order_id as 'Order Id' FROM `order_header` oh
+        $sqlchksubsid = "SELECT oh.`delivery_date`as 'Delivery Date',r.name as 'Client Name',bp.title as 'Item Name',bp.pack_size as 'Pack Size', bp.pack_unit as 'Pack Unit', bp.pack_size_in_gm as 'Pack Size In Gm' ,ol.delivered_qty as 'Total Quantity (Kg)',TRUNCATE((ol.unit_price *ol.delivered_qty),2) as 'Total Amount', oh.`invoice_number` as 'Invoice ID', oh.order_id as 'Order Id', ca.category_name as `Category Name` FROM `order_header` oh
 left join order_line as ol on ol.`order_id`=oh.`order_id` left join cb_dev_groots.retailer r on r.id=oh.user_id
 left join cb_dev_groots.base_product bp on bp.base_product_id=ol.base_product_id
+left join cb_dev_groots.product_category_mapping cpm on bp.base_product_id = cpm.base_product_id		
+left join cb_dev_groots.category ca on ca.category_id = cpm.category_id
 WHERE oh.delivery_date between('".$cDate."') and ('".$cdate1."') and oh.status not in ('Cancelled')";
 	
 //echo $sqlchksubsid; die;
