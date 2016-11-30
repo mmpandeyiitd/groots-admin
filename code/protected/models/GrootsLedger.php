e6d3e6aa8c5dfef759810e3b67f2b032df43d21f
manmohan <mmpandeyiitd@gmail.com>
2016-11-30 13:14:11 +0530

    add order quantity in report

--- code/protected/models/GrootsLedger.php
+++ code/protected/models/GrootsLedger.php
@@ -187,7 +187,7 @@ class GrootsLedger extends CActiveRecord
     public static function downloadCSVByIDs($cDate,$cdate1) {
 
 
-        $sqlchksubsid = "SELECT oh.`delivery_date`as 'Delivery Date',r.name as 'Client Name',bp.title as 'Item Name',bp.pack_size as 'Pack Size', bp.pack_unit as 'Pack Unit', bp.pack_size_in_gm as 'Pack Size In Gm' ,ol.delivered_qty as 'Total Quantity (Kg)',TRUNCATE((ol.unit_price *ol.delivered_qty),2) as 'Total Amount', oh.`invoice_number` as 'Invoice ID', oh.order_id as 'Order Id' FROM `order_header` oh
+        $sqlchksubsid = "SELECT oh.`delivery_date`as 'Delivery Date',r.name as 'Client Name',bp.title as 'Item Name',bp.pack_size as 'Pack Size', bp.pack_unit as 'Pack Unit', bp.pack_size_in_gm as 'Pack Size In Gm' , ol.product_qty*bp.pack_size_in_gm/1000 as 'Order Quantity (Kg)' , ol.delivered_qty*bp.pack_size_in_gm/1000 as 'Delivered Quantity (Kg)',TRUNCATE((ol.unit_price *ol.delivered_qty),2) as 'Total Amount', oh.`invoice_number` as 'Invoice ID', oh.order_id as 'Order Id' FROM `order_header` oh
 left join order_line as ol on ol.`order_id`=oh.`order_id` left join cb_dev_groots.retailer r on r.id=oh.user_id
 left join cb_dev_groots.base_product bp on bp.base_product_id=ol.base_product_id
 WHERE oh.delivery_date between('".$cDate."') and ('".$cdate1."') and oh.status not in ('Cancelled')";
@@ -220,10 +220,8 @@ WHERE oh.delivery_date between('".$cDate."') and ('".$cdate1."') and oh.status n
                 $invoice_no =  INVOICE_TEXT.$deliveryDateArray[0].$deliveryDateArray[1].$values['Order Id'];
                 $values['Invoice ID'] = $invoice_no;
 
-                $delvQuantityInKg = $values['Total Quantity (Kg)'] * $values['Pack Size In Gm']/1000; 
-
-                
-                $values['Total Quantity (Kg)'] = $delvQuantityInKg;
+                /*$delvQuantityInKg = $values['Total Quantity (Kg)'] * $values['Pack Size In Gm']/1000;
+                $values['Total Quantity (Kg)'] = $delvQuantityInKg;*/
                 //print_r($values);die;
                 fputcsv($fp, $values);
             }
