45c01d32506ef056a56d8c4d7682a091b2af81d7
manmohan <mmpandeyiitd@gmail.com>
2017-01-09 19:37:40 +0530

    change in query

--- code/query.sql
+++ code/query.sql
@@ -742,4 +742,6 @@ insert into cb_dev_groots.collection_agent values(null, 'Warehouse', 0, 1);
 insert into cb_dev_groots.groots_employee values(null, 'Narender', null, null, null, null, null, null, 1, 1, CURDATE(), null, 1);
 insert into employee_department values (null,5, 1, CURDATE(), null, 1);
 alter table groots_orders.retailer_payments modify column payment_type enum('Cash','Cheque','DemandDraft','OnlineTransfer','Debit Note','PayTm') NOT NULL DEFAULT 'Cash';
+alter table groots_orders.retailer_payments_log modify column payment_type enum('Cash','Cheque','DemandDraft','OnlineTransfer','Debit Note','PayTm') NOT NULL DEFAULT 'Cash';
 alter table cb_dev_groots.retailer modify column collection_frequency enum('daily','weekly','fortnight','monthly','45-days', '3-days') DEFAULT 'daily';
+alter table cb_dev_groots.retailer_log modify column collection_frequency enum('daily','weekly','fortnight','monthly','45-days', '3-days') DEFAULT 'daily';
