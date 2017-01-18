67fa131e1fd81dbfc98d2a2c30bee00f63cdf196
manmohan <mmpandeyiitd@gmail.com>
2017-01-18 13:52:02 +0530

    echo commented

--- code/protected/utility/EmailClient.php
+++ code/protected/utility/EmailClient.php
@@ -67,7 +67,7 @@ class EmailClient
         try {
             $result = $this->sesClient->sendEmail($request);
             $messageId = $result->get('MessageId');
-            echo("Email sent! Message ID: $messageId"."\n");
+            //echo("Email sent! Message ID: $messageId"."\n");
 
         } catch (Exception $e) {
             echo("The email was not sent. Error message: ");
