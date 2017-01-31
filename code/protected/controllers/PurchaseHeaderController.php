b1653d252390f7abd2cb0c92592637f3d84a05d1
ashu dogra <ashudogra20@gmail.com>
2017-01-31 12:11:32 +0530

    report

--- code/protected/controllers/PurchaseHeaderController.php
+++ code/protected/controllers/PurchaseHeaderController.php
@@ -750,7 +750,7 @@ public static function createProcurementOrder($purchaseOrderMap, $date, $w_id){
 
 
     public function actionDownloadReportById($id){
-        $sql = 'select pl.*,bp.title,bp.grade, case when pl.vendor_id = 0 then "parent" when pl.vendor_id != 0 then v.name end as vendorName from purchase_line pl  left join cb_dev_groots.vendors as v on v.id = pl.vendor_id 
+        $sql = 'select pl.*,bp.title,bp.grade, pl.urd_number, case when pl.vendor_id = 0 then "" when pl.vendor_id != 0 then v.name end as vendorName from purchase_line pl  left join cb_dev_groots.vendors as v on v.id = pl.vendor_id 
         left join cb_dev_groots.base_product as bp on pl.base_product_id = bp.base_product_id
         left join cb_dev_groots.product_category_mapping pcm on pcm.base_product_id=bp.base_product_id
          where pl.purchase_id = '.$id.' group by pl.base_product_id order by pcm.category_id asc, bp.base_title asc, bp.priority asc' ;
