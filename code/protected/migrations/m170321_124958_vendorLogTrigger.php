8de09310455fc0e0a6891240b01c1efa716fca2c
Ashu Dogra <ashudogra20@gmail.com>
2017-03-24 15:57:16 +0530

    change in trigger syntax

--- code/protected/migrations/m170321_124958_vendorLogTrigger.php
+++ code/protected/migrations/m170321_124958_vendorLogTrigger.php
@@ -13,7 +13,7 @@ class m170321_124958_vendorLogTrigger extends CDbMigration
             $currentPendingDate = $command->queryScalar();
             $triggerInsert = 'create trigger vendor_log_insert after insert on cb_dev_groots.vendors for each row
                             insert into vendor_log(vendor_id, total_pending,base_date, created_at, updated_at)
-                            values (NEW.id, sum(NEW.total_pending_amount, NEW.initial_pending_amount), "'.$currentPendingDate.'", CURRENTDATETIME(), CURRENTDATETIME())';
+                            values (NEW.id, NEW.total_pending_amount + NEW.initial_pending_amount, "'.$currentPendingDate.'", CURRENTDATETIME(), CURRENTDATETIME())';
             $this->execute($triggerInsert);
             $sql = 'insert into cb_dev_groots.vendor_log (vendor_id , total_pending, base_date, created_at, updated_at) (select id, initial_pending_date, "2017-02-28", CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP() from cb_dev_groots.vendors as v where v.id > 37 )' ;
             $this->execute($sql);
